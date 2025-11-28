163328b39611b560d4e991dde6dbd3cda123cc6d
Sreehari <sreeharimangalasseri528@gmail.com>
2025-11-28 09:54:12 +0530

    weather-assets-shown

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
+import WeatherBackground from "./components/WeatherBackground";
+WeatherBackground;
 
 const App = () => {
-  return (
-    <div>
-        <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </div>
-  )
-}
 
-export default App
+  const [weather, setWeather] = useState(null);
+  
+  // this function check weather exissts or not and return and object
+  const getWeatherCondition = () =>
+    weather && {
+      main: weather.weather[0].main,
+      isDay:
+        Date.now() / 1000 > weather.sys.sunrise &&
+        Date.now() / 1000 < weather.sys.susnset,
+    };
+
+  return (
+    <div className="min-h-screen">
+      <WeatherBackground condition={getWeatherCondition()} />
+
+      <div className="flex items-center justify-center p-6 min-h-screen ">
+        <div className="bg-transparent backdrop-filter backdrop-blur-md rounded-xl shadow-2xl p-8 max-w-md text-white w-full border-white/30 relative z-10">
+          <h1 className="text-4xl font-extrabold text-center mb-6">
+            Weather App
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
