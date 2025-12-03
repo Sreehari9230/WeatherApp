24e55ff54d821f7e4baae1d50baa1bb3ffd9a33b
Sreehari <sreeharimangalasseri528@gmail.com>
2025-12-03 22:37:53 +0530

    37-34

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,74 @@
-import React, { useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import WeatherBackground from "./components/WeatherBackground";
 WeatherBackground;
+import { convertTemperature } from "./components/Helper";
 
 const App = () => {
-
   const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState("");
+  const [suggestion, setSuggestion] = useState("");
+  const [unit, setUnit] = useState("");
+  const [error, setError] = useState("");
+
+  const API_KEY = "2e293f16e901d3dfc9e3fd738a7006a4";
+
+  // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API_KEY}
+
+  // http://api.openweathermap.org/geo/1.0/direct?q={query}&limit={5}&appid={API_KEY}
+
+  useEffect(() => {
+    if (city.trim().length >= 3 && !weather) {
+      const timer = setTimeout(() => {
+        fetchSuggestions(city), 500;
+      }, 500);
+      return () => clearTimeout(timer);
+    }
+    setSuggestion([]);
+  }, [city, weather]);
+
+  // fetch five location suggestion and update on ui
+  const fetchSuggestions = async (query) => {
+    try {
+      const res = await fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit={5}&appid=${API_KEY}`
+      );
+      res.ok ? setSuggestion(await res.json()) : setSuggestion([]);
+    } catch (error) {
+      setSuggestion([]);
+    }
+  };
+
+  //  thiswill fetch data from url
+  const fetchWeatherData = async (url, name = "") => {
+    setError("");
+    setWeather(null);
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error((await response.json()).message || "City not found");
+        const data = await response.json();
+        setWeather(data);
+        setCity(name || data.name);
+        setSuggestion([]);
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  // this funtion will prevent form submission vaidates city and fetchwes data via api key
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    if (!city.trim()) {
+      return setError("Please enter a city name");
+    }
+    await fetchWeatherData(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city.trim()}&appid=${API_KEY}&units=metric`
+    );
+  };
+
+  // https://api.openweathermap.org/data/2.5/weather?lat=${s.lat}&lon=${s.lon}&appid={API_KEY}&units=metric
 
   // this function check weather exissts or not and return and object
   const getWeatherCondition = () =>
@@ -24,6 +88,77 @@ const App = () => {
           <h1 className="text-4xl font-extrabold text-center mb-6">
             Weather App
           </h1>
+
+          {weather ? (
+            <form onSubmit={handleSearch} className="flrx flex-col relative">
+              <input
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+                placeholder="Enter City or Country"
+                className="mb-4 p-3 rounded border border-white bg-transparent text-white placeholder-white focus:outline-none focus:border-blue-300 transition duration-300"
+              />{" "}
+              //minmum 3 lettrets for suggestion
+              {suggestion.length > 0 && (
+                <div className="absolute top-12 left-0 right-0 bg-transparent shadow-md rounded z-10 ">
+                  {suggestion.map((s) => {
+                    <button
+                      type="button"
+                      key={`${s.lat} - ${s.lon}`}
+                      onClick={() => {
+                        fetchWeatherData(
+                          `https://api.openweathermap.org/data/2.5/weather?lat=${s.lat}&lon=${s.lon}&appid={API_KEY}&units=metric`,
+                          `${s.name}, ${s.country}, ${
+                            s.state ? `${s.state}` : ""
+                          }`
+                        );
+                      }}
+                      className="block hover:bg-blue-700 bg-transparent px-4 py-2 text-sm text-left w-full transition-colors"
+                    >
+                      {s.name},{s.country}, {s.state && `, ${s.state}`}
+                    </button>;
+                  })}
+                </div>
+              )}
+              <button
+                type="submit"
+                className="bg-purple-700 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded transition-colors"
+              >
+                Get WEATHER
+              </button>
+            </form>
+          ) : (
+            <div className="mt-6 text-center transition-opacity duration-300">
+              <button
+                onClick={() => {
+                  setWeather(null);
+                  setCity("");
+                }}
+                className="mb-4 bg-purple-900 hover:bg-blue-700 text-white font-semibold py-1 px-3 rounded transition-colors"
+              >
+                New Search
+              </button>
+
+              <div className="flex justify-between items-center">
+                <h2 className="text-3xl font-bold">{weather.name}</h2>
+                <button
+                  onClick={() => setUnit((u) => (u === "C" ? "F" : "C"))}
+                  className="bg-blue-700 hover:bg-blue-800 text-white font-semibold py-1 px-3 rounded transition-colors"
+                >
+                  &deg;{unit}
+                </button>
+              </div>
+
+              <img
+                src={`https://api.openweathermap.org/img/wm/${weather.weather[0].icon}@2px.png`}
+                alt={weather.weather[0].description}
+                className="mx-auto my-4 animate-bounce"
+              />
+              <p className="text-4xl ">
+                {convertTemperature(weather.main.temp, unit)} &deg;{unit}
+              </p>
+              <p className="capitalize">{weather.weather[0].description}</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
